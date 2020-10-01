a2f43959e49494d2f6f2d9ee227ee4c5eee005cf
Adam Boerhave <arboerhave@gmail.com>
2020-10-01 14:53:46 -0500

    added a button

--- client.js
+++ client.js
@@ -5,6 +5,16 @@ $(document).ready(onReady);
 
 function onReady(number) {
     console.log('jq', number);   
+
+    // get element from html
+    // the third event
+    // event listener
+    $('#bestButton').on('click', buttonClicked);
+}
+
+function buttonClicked() {
+    console.log('I was clicked');
+    
 }
 
 // event #1 before it even gets to the document ready on line 3
